276236079b5267fa51a2b0435e762a690a50af14
muschellij2 <muschellij2@gmail.com>
2018-08-16 18:15:56 -0400

    Added example with &.

--- R/create_ssml.R
+++ R/create_ssml.R
@@ -11,10 +11,12 @@
 #' @export
 #'
 #' @examples
+#' create_ssml("hey I really like things & dogs", escape = TRUE)
 #' create_ssml("hey I really like things")
 #' create_ssml('hey I <emphasis level="strong">really like</emphasis> things')
 #' create_ssml('hey I <emphasis level="strong">really like</emphasis> things',
 #' escape = TRUE)
+#'
 create_ssml = function(
   script,
   gender = c("Female", "Male"),
