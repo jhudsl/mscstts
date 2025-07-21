8522886158e460bda511b7f1da90ee6876df6085
muschellij2 <muschellij2@gmail.com>
2025-07-21 14:18:00 -0400

    fixing typo in docs for language

--- R/ms_locale_names.R
+++ R/ms_locale_names.R
@@ -2,7 +2,7 @@
 #'
 #' @format A \code{data.frame} with 2 columns:
 #' \describe{
-#' \item{code}{the 5 character langugae code, separated by a hyphen, also referred to as a locale}
+#' \item{code}{the 5 character language code, separated by a hyphen, also referred to as a locale}
 #' \item{language}{the name of the language}
 #' }
 "ms_locale_names"
@@ -17,7 +17,7 @@
 #' \item{local_name}{local name}
 #' \item{short_name}{shorter than locale name, but has language code as well}
 #' \item{gender}{gender of the voice}
-#' \item{code}{the 5 character langugae code, separated by a hyphen, also referred to as a locale}
+#' \item{code}{the 5 character language code, separated by a hyphen, also referred to as a locale}
 #' \item{sample_rate}{sample rate (in Hz) of the voice}
 #' \item{voice_type}{Standard or Neural}
 #' }
